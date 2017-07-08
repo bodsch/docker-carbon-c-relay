c2ca89c62cc3b870ba354d8db647a17c003aa6b6
Bodo Schulz <bodo@boone-schulz.de>
2017-07-08 19:51:43 +0200

    reorganizie init script

--- rootfs/init/run.sh
+++ rootfs/init/run.sh
@@ -15,19 +15,6 @@ createConfig() {
 
 }
 
-startSupervisor() {
-
-  echo -e "\n Starting Supervisor.\n\n"
-
-  if [ -f /etc/supervisord.conf ]
-  then
-    /usr/bin/supervisord -c /etc/supervisord.conf >> /dev/null
-  else
-    exec /bin/sh
-  fi
-}
-
-
 run() {
 
   createConfig
